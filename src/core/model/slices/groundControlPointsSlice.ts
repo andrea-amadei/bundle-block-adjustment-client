ae62477abb1068d84df8693b9166082c28dd0124
andrea-amadei <andrea1.amadei@mail.polimi.it>
2022-03-22 12:54:38 +0100

    Add GCP Slice

--- src/core/model/slices/groundControlPointsSlice.ts
+++ src/core/model/slices/groundControlPointsSlice.ts
@@ -1,10 +1,10 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { PointOnImage } from '../interfaces/pointOnImageInterface';
 import type { RootState } from '../store';
 
-type PointOnImageGCP = { PointOnImage: 'MANUAL' | 'IMPORTED' } & PointOnImage;
+export type PointOnImageGCP = { source: 'MANUAL' | 'IMPORTED' } & PointOnImage;
 
-interface GroundControlPoint {
+export interface GroundControlPoint {
   pointId: number;
   linkedImages: PointOnImageGCP[];
   x: number;
@@ -12,32 +12,130 @@ interface GroundControlPoint {
   z: number;
 }
 
-export interface GroundControlPointsState {
-  groundControlPointsList: GroundControlPoint[];
-}
-
-const initialState = {
-  groundControlPointsList: [],
-} as GroundControlPointsState;
-
 export const groundControlPointsSlice = createSlice({
   name: 'groundControlPoints',
-  initialState,
 
-  // TODO: add reducers
-  reducers: {},
+  // TODO: perhaps it is better to convert the state to a map?
+  initialState: [] as GroundControlPoint[],
+
+  reducers: {
+    addPoint: (state: GroundControlPoint[], action: PayloadAction<GroundControlPoint>) => {
+      if (state.map((x) => x.pointId).includes(action.payload.pointId))
+        throw Error('Point ID must be unique');
+
+      state.push(action.payload);
+    },
+
+    removePointByPointId: (state: GroundControlPoint[], action: PayloadAction<number>) => {
+      state.splice(
+        state.findIndex((x) => x.pointId === action.payload),
+        1
+      );
+    },
+
+    setXByPointId: {
+      prepare: (id: number, x: number) => ({ payload: { id, x } }),
+
+      reducer: (state: GroundControlPoint[], action: PayloadAction<{ id: number; x: number }>) => {
+        try {
+          state.filter((p) => p.pointId === action.payload.id)[0].x =
+            action.payload.x;
+        } catch (e) {
+          throw Error('No existing point with given ID');
+        }
+      },
+    },
+
+    setYByPointId: {
+      prepare: (id: number, y: number) => ({ payload: { id, y } }),
+
+      reducer: (state: GroundControlPoint[], action: PayloadAction<{ id: number; y: number }>) => {
+        try {
+          state.filter((p) => p.pointId === action.payload.id)[0].y =
+            action.payload.y;
+        } catch (e) {
+          throw Error('No existing point with given ID');
+        }
+      },
+    },
+
+    setZByPointId: {
+      prepare: (id: number, z: number) => ({ payload: { id, z } }),
+
+      reducer: (state: GroundControlPoint[], action: PayloadAction<{ id: number; z: number }>) => {
+        try {
+          state.filter((p) => p.pointId === action.payload.id)[0].z =
+            action.payload.z;
+        } catch (e) {
+          throw Error('No existing point with given ID');
+        }
+      },
+    },
+
+    addLinkedImageByPointId: {
+      prepare: (id: number, image: PointOnImageGCP) => ({
+        payload: { id, image },
+      }),
+
+      reducer: (state: GroundControlPoint[], action: PayloadAction<{ id: number, image: PointOnImageGCP }>) => {
+        if (!state.map((x) => x.pointId).includes(action.payload.id))
+          throw Error('No existing point with given ID');
+
+        if (action.payload.id !== action.payload.image.pointId)
+          throw Error("Point ID and image's point ID must be identical");
+
+        const point = state.filter((p) => p.pointId === action.payload.id)[0];
+
+        if (
+          point.linkedImages
+            .map((x) => x.imageId)
+            .includes(action.payload.image.imageId)
+        )
+          throw Error('Image ID must be unique');
+
+        point.linkedImages.push(action.payload.image);
+      },
+    },
+
+    removeLinkedImageByPointId: {
+      prepare: (pointId: number, imageId: number) => ({
+        payload: { pointId, imageId },
+      }),
+
+      reducer: (state: GroundControlPoint[], action: PayloadAction<{ pointId: number, imageId: number }>) => {
+        if (!state.map((x) => x.pointId).includes(action.payload.pointId))
+          throw Error('No existing point with given ID');
+
+        const images = state.filter(
+          (p) => p.pointId === action.payload.pointId
+        )[0].linkedImages;
+
+        images.splice(
+          images.findIndex((p) => p.imageId === action.payload.imageId),
+          1
+        );
+      },
+    },
+  },
 });
 
-// TODO: add reducers
-// export const { } = groundControlPointsSlice.actions;
+export const {
+  addPoint,
+  removePointByPointId,
+  setXByPointId,
+  setYByPointId,
+  setZByPointId,
+  addLinkedImageByPointId,
+  removeLinkedImageByPointId,
+} = groundControlPointsSlice.actions;
 
 export const selectGroundControlPointById =
   (id: number) => (state: RootState) =>
-    state.groundControlPoints.groundControlPointsList[id];
+    state.groundControlPoints[id];
 
-export const selectGroundControlPointListInImage =
+export const selectGroundControlPointsOnImage =
   (imageId: number) => (state: RootState) =>
-    state.groundControlPoints.groundControlPointsList.filter((x) =>
+    state.groundControlPoints.filter((x) =>
       x.linkedImages.some((y) => y.imageId === imageId)
     );
 
