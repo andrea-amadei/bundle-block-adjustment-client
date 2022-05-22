be33ffa2412fb0d54798157fee0a0fd4cec42281
andrea-amadei <andrea1.amadei@mail.polimi.it>
2022-05-23 00:59:24 +0200

    Add points, zooming and scrolling to image editor

--- src/ui/components/ImageEditor.tsx
+++ src/ui/components/ImageEditor.tsx
@@ -1,39 +1,136 @@
-import "./ImageEditor.scss"
+import './ImageEditor.scss';
 
-import { useParams } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { selectImageById } from "../../core/model/slices/imageListSlice";
-import { selectAllTiePoints } from "../../core/model/slices/tiePointsSlice";
-import { selectAllGroundControlPoints } from "../../core/model/slices/groundControlPointsSlice";
-import { useEffect } from "react";
+import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { useEffect, useLayoutEffect, useRef, useState } from 'react';
+import WZoom from 'vanilla-js-wheel-zoom';
+import { selectImageById } from '../../core/model/slices/imageListSlice';
+import { selectAllTiePoints, selectTiePointsOnImage } from '../../core/model/slices/tiePointsSlice';
+import { selectAllGroundControlPoints } from '../../core/model/slices/groundControlPointsSlice';
+import { PointMarker } from './PointMarker';
 
-export function ImageEditor(props) {
-  const {selectedImageId, selectedPointType, selectedPointId} = useParams();
+// eslint-disable-next-line import/prefer-default-export
+export function ImageEditor() {
+  const { selectedImageId } = useParams();
 
-  if(!selectedImageId)
-    throw new Error("No image selected!");
+  if (!selectedImageId) throw new Error('No image selected!');
 
   const selectedImage = useSelector(selectImageById(selectedImageId));
-  const tpList = useSelector(selectAllTiePoints);
+  const tpList = useSelector(
+    selectTiePointsOnImage(parseInt(selectedImageId, 10))
+  );
   const gcpList = useSelector(selectAllGroundControlPoints);
 
-  useEffect(
-    () => console.log(selectedImageId, selectedImage),
-    [selectedImage, selectedImageId]
-  );
+  const canvasRef = useRef(null);
+  const groupRef = useRef(null);
+  const imgRef = useRef(null);
+  const inputRangeRef = useRef(null);
 
-  if(selectedImage)
+  let [wzoom, setWzoom] = useState(null);
+  let [zoomValue, setZoomValue] = useState(1);
+  let [isLocked, setLocked] = useState(true);
+
+  let scale;
+
+  const handleKeyDown = (event: KeyboardEvent) => {
+    switch (event.code) {
+      case 'NumpadAdd':
+        wzoom?.zoomUp();
+        break;
+      case 'NumpadSubtract':
+        wzoom?.zoomDown();
+        break;
+      default:
+        break;
+    }
+  };
+
+  useLayoutEffect(() => {
+    scale = 0.5 * Math.min(
+        canvasRef.current.clientWidth / groupRef.current.clientWidth,
+        canvasRef.current.clientHeight / groupRef.current.clientHeight
+      );
+
+    setWzoom(
+      WZoom.create('.editor-group', {
+        type: 'html',
+        zoomOnClick: false,
+        smoothExtinction: 0,
+        minScale: scale,
+        maxScale: 30,
+        speed: 1 / scale,
+        watchImageChange: false,
+        dragScrollable: true,
+        dragScrollableOptions: {
+          smoothExtinction: 0,
+          onGrab() {
+            canvasRef.current.style.cursor = 'grabbing';
+          },
+          onDrop() {
+            canvasRef.current.style.cursor = 'grab';
+          },
+        },
+      })
+    );
+  }, [canvasRef]);
+
+  const initialize = () => {
+    if (wzoom != null) {
+      wzoom.prepare();
+      wzoom.options.rescale = () => setZoomValue(wzoom?.content.currentScale);
+      wzoom.zoomUp();
+
+      inputRangeRef.current.min = wzoom.options.minScale;
+      inputRangeRef.current.defaultValue = wzoom.options.minScale;
+      inputRangeRef.current.max = wzoom.options.maxScale;
+      inputRangeRef.current.step = 1 / wzoom.options.speed;
+      setZoomValue(wzoom.content.currentScale);
+
+      window.addEventListener('keypress', handleKeyDown);
+
+      groupRef.current.removeEventListener('mousedown', wzoom.dragScrollable._grabHandler);
+      groupRef.current.addEventListener('mousedown', wzoom.dragScrollable._grabHandler);
+    }
+  };
+
+  useEffect(() => {
+    initialize();
+
+    window.addEventListener('keypress', handleKeyDown);
+    return () => window.removeEventListener('keypress', handleKeyDown);
+  }, [wzoom]);
+
+  if (selectedImage)
     return (
       <div className="editor-img-container">
-        <div className="img-title">
-          { `[${selectedImage.id}] ${selectedImage.name}` }
+        <div className="img-header">
+          <div className="img-title">
+            {`[${selectedImage.id}] ${selectedImage.name}`}
+          </div>
+          <div className="img-controls">
+            <button style={{ marginRight: '5%' }} onClick={() => setLocked(!isLocked)}>{isLocked ? 'X' : '_'}</button>
+            <span className="img-controls-label">Zoom</span>
+            <input type="range" ref={inputRangeRef} value={zoomValue} onInput={() =>
+                Number(inputRangeRef.current.value) > wzoom.content.currentScale
+                  ? wzoom.zoomUp()
+                  : wzoom.zoomDown()
+              }
+            />
+            <button onClick={() => wzoom?.zoomUp()}>+</button>
+            <button onClick={() => wzoom?.zoomDown()}>-</button>
+          </div>
         </div>
-        <img className="editor-img" src={selectedImage.path} alt={selectedImage.path}/>
+        <div className="editor-canvas" ref={canvasRef}>
+          <div className="editor-group" ref={groupRef}>
+            <img className="editor-img" ref={imgRef} src={selectedImage.path} alt={selectedImage.path}/>
+            <div className="editor-points">
+              {tpList.map((tp) => (
+                <PointMarker point={tp} zoomValue={zoomValue} wzoom={wzoom} type="TP" isMovable={!isLocked} />
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
     );
-  else
-    return (
-      <div>Loading...</div>
-    );
-
+  return <div>Loading...</div>;
 }
---
+++ src/ui/components/PointMarker.tsx
@@ -0,0 +1,135 @@
+import './PointMarker.scss';
+
+import { useEffect, useState } from 'react';
+import { PointOnImage } from '../../core/model/slices/common/interfaces';
+import { store } from '../../core/model/store';
+import { setLinkedPointX as setLinkedPointX_TP, setLinkedPointY as setLinkedPointY_TP } from '../../core/model/slices/tiePointsSlice';
+import { setLinkedPointX as setLinkedPointX_GCP, setLinkedPointY as setLinkedPointY_GCP } from '../../core/model/slices/groundControlPointsSlice';
+
+interface PointMarkerAttributes {
+  point: PointOnImage;
+  type: 'TP' | 'GCP';
+  isMovable?: boolean;
+  zoomValue: number;
+  wzoom: any;
+}
+
+// eslint-disable-next-line import/prefer-default-export
+export function PointMarker(props: PointMarkerAttributes) {
+  const { point, type, isMovable, zoomValue, wzoom } = props;
+
+  if (type === undefined) throw Error('Attribute "type" is required');
+
+  const [dragged, setDragged] = useState('none');
+
+  const [mouseStartingX, setMouseStartingX] = useState(0);
+  const [mouseStartingY, setMouseStartingY] = useState(0);
+  const [pointStartingX, setPointStartingX] = useState(0);
+  const [pointStartingY, setPointStartingY] = useState(0);
+
+  const handleMouseMovement = (e) => {
+    document.removeEventListener(
+      'mousemove',
+      wzoom.dragScrollable._moveHandler
+    );
+
+    if (dragged === 'all' || dragged === 'right') {
+      if (type === 'TP')
+        store.dispatch(
+          setLinkedPointX_TP(
+            point.pointId,
+            point.imageId,
+            pointStartingX + Math.floor((e.pageX - mouseStartingX) / zoomValue)
+          )
+        );
+      else
+        store.dispatch(
+          setLinkedPointX_GCP(
+            point.pointId,
+            point.imageId,
+            pointStartingX + Math.floor((e.pageX - mouseStartingX) / zoomValue)
+          )
+        );
+    }
+
+    if (dragged === 'all' || dragged === 'bottom') {
+      if (type === 'TP')
+        store.dispatch(
+          setLinkedPointY_TP(
+            point.pointId,
+            point.imageId,
+            pointStartingY + Math.floor((e.pageY - mouseStartingY) / zoomValue)
+          )
+        );
+      else
+        store.dispatch(
+          setLinkedPointY_GCP(
+            point.pointId,
+            point.imageId,
+            pointStartingY + Math.floor((e.pageY - mouseStartingY) / zoomValue)
+          )
+        );
+    }
+  };
+
+  const _handleMouseMovement = (e) => handleMouseMovement(e);
+
+  const handleMouseHold = (e, dragType: 'right' | 'bottom' | 'all') => {
+    if (e.type === 'mousedown') {
+      setDragged(dragType);
+      setMouseStartingX(e.pageX);
+      setMouseStartingY(e.pageY);
+      setPointStartingX(point.x);
+      setPointStartingY(point.y);
+    } else {
+      setDragged('none');
+    }
+  };
+
+  const _handleMouseHold = (e) => handleMouseHold(e, 'all');
+
+  useEffect(() => {
+    document.addEventListener('mouseup', _handleMouseHold);
+
+    return () => {
+      document.removeEventListener('mouseup', _handleMouseHold);
+    };
+  });
+
+  useEffect(() => {
+    if (dragged !== 'none') {
+      document.addEventListener('mousemove', _handleMouseMovement);
+    }
+
+    return () => {
+      document.removeEventListener('mousemove', _handleMouseMovement);
+    };
+  }, [dragged]);
+
+  return (
+    <svg className="point-marker" viewBox="0 0 200 200" width="30px" height="30px" style={{left: point.x - 8, top: point.y - 8, position: 'absolute'}} transform="scale(NaN)">
+      <circle cx="50" cy="50" r="1" stroke="white" strokeWidth="1" fill="black" />
+      {isMovable ? (
+        <>
+          <g onMouseDown={(e) => handleMouseHold(e, 'right')} visibility={dragged === 'none' || dragged === 'right' ? 'visible' : 'hidden'}>
+            <rect x="95" y="30" width="85" height="40" fillOpacity="0" />
+            <line x1='95' y1='50' x2='160' y2='50' stroke='black' strokeWidth='8'/>
+            <polygon points="160 70, 180 50, 160 30" stroke="black" />
+          </g>
+          <g onMouseDown={(e) => handleMouseHold(e, 'bottom')} visibility={dragged === 'none' || dragged === 'bottom' ? 'visible' : 'hidden'}>
+            <rect x="30" y="95" width="40" height="85" fillOpacity="0" />
+            <line x1='50' y1='95' x2='50' y2='160' stroke='black' strokeWidth='8'/>
+            <polygon points="70 160, 50 180, 30 160" stroke="black" />
+          </g>
+        </>
+      ) : (
+        <></>
+      )}
+      <circle className={`outer-circle point-type-${type.toLowerCase()} dragged-${dragged}`} cx="50" cy="50" r="40" strokeWidth="15" fillOpacity="0" onMouseDown={(e) => isMovable ? handleMouseHold(e, 'all') : null} />
+    </svg>
+  );
+}
+
+PointMarker.defaultProps = {
+  isMovable: false,
+};
