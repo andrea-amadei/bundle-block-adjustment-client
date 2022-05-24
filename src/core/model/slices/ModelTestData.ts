488c1bcced728d12cd636a8124d271424a8146dc
andrea-amadei <andrea1.amadei@mail.polimi.it>
2022-05-24 20:05:08 +0200

    Update test data

--- src/core/model/slices/ModelTestData.ts
+++ src/core/model/slices/ModelTestData.ts
@@ -1,12 +1,31 @@
-import { TiePoint } from "./tiePointsSlice";
-
+import { TiePoint } from './tiePointsSlice';
+import { GroundControlPoint } from './groundControlPointsSlice';
 
 export const imgTest = {
-  1: {id: 1, name: "Img1", path: "https://images.unsplash.com/photo-1648818172118-093be9c12d59?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"},
-  2: {id: 2, name: "Img2", path: "https://images.unsplash.com/photo-1615472669810-e72da447b314?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=749&q=80"},
-  3: {id: 3, name: "Img3", path: "https://images.unsplash.com/photo-1611363279288-55e33723ac06?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"},
-  4: {id: 4, name: "Vaporwave", path: "https://i.redd.it/1iwcit1gidyy.jpg"},
-  5: {id: 5, name: "Random", path: "https://www.dcode.fr/tools/image-randomness/images/random-dcode.png"},
+  1: {
+    id: 1,
+    name: 'Img1',
+    path: 'https://images.unsplash.com/photo-1648818172118-093be9c12d59?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+  },
+  2: {
+    id: 2,
+    name: 'Img2',
+    path: 'https://images.unsplash.com/photo-1615472669810-e72da447b314?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=749&q=80',
+  },
+  3: {
+    id: 3,
+    name: 'Img3',
+    path: 'https://images.unsplash.com/photo-1611363279288-55e33723ac06?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+  },
+  4: { id: 4,
+    name: 'Vaporwave',
+    path: 'https://i.redd.it/1iwcit1gidyy.jpg'
+  },
+  5: {
+    id: 5,
+    name: 'Random',
+    path: 'https://www.dcode.fr/tools/image-randomness/images/random-dcode.png',
+  },
 };
 
 export const tpTest: TiePoint[] = [
@@ -34,7 +53,7 @@ export const tpTest: TiePoint[] = [
         imageId: 3,
         source: 'MANUAL',
       },
-    ]
+    ],
   },
   {
     pointId: 2,
@@ -44,7 +63,7 @@ export const tpTest: TiePoint[] = [
         x: 4,
         y: 8,
         imageId: 1,
-        source: 'MANUAL',
+        source: 'IMPORTED',
       },
       {
         pointId: 2,
@@ -52,8 +71,8 @@ export const tpTest: TiePoint[] = [
         y: 16,
         imageId: 2,
         source: 'MANUAL',
-      }
-    ]
+      },
+    ],
   },
   {
     pointId: 3,
@@ -64,8 +83,8 @@ export const tpTest: TiePoint[] = [
         y: 30,
         imageId: 1,
         source: 'MANUAL',
-      }
-    ]
+      },
+    ],
   },
   {
     pointId: 1000,
@@ -76,8 +95,8 @@ export const tpTest: TiePoint[] = [
         y: 30,
         imageId: 5,
         source: 'IMPORTED',
-      }
-    ]
+      },
+    ],
   },
   {
     pointId: 1001,
@@ -88,8 +107,25 @@ export const tpTest: TiePoint[] = [
         y: 20,
         imageId: 5,
         source: 'IMPORTED',
-      }
-    ]
+      },
+    ],
   },
-]
+];
 
+export const gcpTest: GroundControlPoint[] = [
+  {
+    pointId: 1,
+    x: 20,
+    y: 40,
+    z: 60,
+    linkedPoints: [
+      {
+        pointId: 1,
+        x: 180,
+        y: 180,
+        imageId: 1,
+        source: 'MANUAL',
+      },
+    ],
+  },
+];
