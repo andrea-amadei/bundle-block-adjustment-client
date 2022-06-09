f3b931131a9606ed511eec0bbdc50d7a75c0741d
Simone Baselice <s.baselice@simonebaselice.it>
2022-06-09 18:03:25 +0200

    Enable add new point button

--- src/ui/components/editor/sidelist/SideListTP.tsx
+++ src/ui/components/editor/sidelist/SideListTP.tsx
@@ -1,7 +1,11 @@
 import { useSelector } from 'react-redux';
 import { useParams, useSearchParams } from 'react-router-dom';
-import { selectTiePointsOnImageBySourceType } from '../../../../core/model/slices/tiePointsSlice';
+import {
+  addNewPointWithLinkedImage,
+  selectTiePointsOnImageBySourceType
+} from "../../../../core/model/slices/tiePointsSlice";
 import { SideList } from './SideList';
+import { store } from "../../../../core/model/store";
 
 export function SideListTP() {
   const [searchParams, setSearchParams] = useSearchParams();
@@ -20,5 +24,5 @@ export function SideListTP() {
       selectTiePointsOnImageBySourceType(imgId, 'AUTO')
     ),
   };
-  return <SideList pointType="TP" points={tpListBySource} />;
+  return <SideList pointType="TP" points={tpListBySource} addPoint={() => store.dispatch(addNewPointWithLinkedImage(selectedImageId))}/>;
 }
