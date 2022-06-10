36032686449aca3b6bf017b133d0db8f2e543457
Simone Baselice <s.baselice@simonebaselice.it>
2022-06-11 00:12:53 +0200

    Add scroll bar to SideList

--- src/ui/components/editor/sidelist/SideList.tsx
+++ src/ui/components/editor/sidelist/SideList.tsx
@@ -53,40 +53,42 @@ export const SideList: React.FC<PropType> = (
   return (
     <div className="point-side-list-container">
       <div className="point-side-list-header">{compactBtn}</div>
-      <div className="source-groups-container">
-        {Object.entries(points)
-          .filter(([source, pointList]) => pointList.length > 0 || source === 'manual')
-          .map(([source, pointList]) => (
-            <div className="source-group" key={source}>
-              <div className="group-header">
-                {POINTS_SOURCE_LABEL[source as keyof typeof POINTS_SOURCE_LABEL]}
-                {source === 'manual' &&
-                  <div className="add-btn" onClick={addPoint}>
-                    <span className="material-symbols-outlined btn"> add </span>
-                  </div>
-                }
+      <div className="source-groups-container-wrapper">
+        <div className="source-groups-container">
+          {Object.entries(points)
+            .filter(([source, pointList]) => pointList.length > 0 || source === 'manual')
+            .map(([source, pointList]) => (
+              <div className="source-group" key={source}>
+                <div className="group-header">
+                  {POINTS_SOURCE_LABEL[source as keyof typeof POINTS_SOURCE_LABEL]}
+                  {source === 'manual' &&
+                    <div className="add-btn" onClick={addPoint}>
+                      <span className="material-symbols-outlined btn"> add </span>
+                    </div>
+                  }
+                </div>
+                { pointList.length > 0 && <hr className="divider" /> }
+                <div className={`point-side-list ${isCompact ? 'compact' : ''}`}>
+                  {pointList.map((pointOnImg) => (
+                    <PointSummary
+                      type={pointType}
+                      id={pointOnImg.pointId}
+                      additionalInfo={{
+                        'key-hidden': `(${pointOnImg.x}, ${pointOnImg.y})`,
+                      }}
+                      compact={isCompact}
+                      key={pointOnImg.pointId}
+                      showOptions={showOptionsForPointId === pointOnImg.pointId}
+                      enableShowOptions={() => setShowOptionsForPointId(pointOnImg.pointId)}
+                      disableShowOptions={() => setShowOptionsForPointId(null)}
+                      unlinkPoint={() => unlinkPoint(pointOnImg.pointId, pointOnImg.imageId)}
+                      deletePoint={() => deletePoint(pointOnImg.pointId)}
+                    />
+                  ))}
+                </div>
               </div>
-              { pointList.length > 0 && <hr className="divider" /> }
-              <div className={`point-side-list ${isCompact ? 'compact' : ''}`}>
-                {pointList.map((pointOnImg) => (
-                  <PointSummary
-                    type={pointType}
-                    id={pointOnImg.pointId}
-                    additionalInfo={{
-                      'key-hidden': `(${pointOnImg.x}, ${pointOnImg.y})`,
-                    }}
-                    compact={isCompact}
-                    key={pointOnImg.pointId}
-                    showOptions={showOptionsForPointId === pointOnImg.pointId}
-                    enableShowOptions={() => setShowOptionsForPointId(pointOnImg.pointId)}
-                    disableShowOptions={() => setShowOptionsForPointId(null)}
-                    unlinkPoint={() => unlinkPoint(pointOnImg.pointId, pointOnImg.imageId)}
-                    deletePoint={() => deletePoint(pointOnImg.pointId)}
-                  />
-                ))}
-              </div>
-            </div>
-          ))}
+            ))}
+        </div>
       </div>
     </div>
   );
